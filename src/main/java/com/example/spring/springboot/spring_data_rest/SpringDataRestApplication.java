6fde195c1572843fd1b90d49b7a605d9327154fe
Mirpribili <mirpribili@ya.ru>
2022-04-01 16:32:16 +0300

    add Spring Boot Actuator

--- src/main/java/com/example/spring/springboot/spring_data_rest/SpringDataRestApplication.java
+++ src/main/java/com/example/spring/springboot/spring_data_rest/SpringDataRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDataRestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringDataRestApplication.class, args);
 	}
 
